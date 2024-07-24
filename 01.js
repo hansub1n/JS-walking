812b58d7f4346e95475066e75884a381de8eaf09
Subin, Han <ella0411@naver.com>
2024-07-24 11:51:17 +0900

    init

---
+++ 01.js
@@ -0,0 +1,7 @@
+const greeting = "Hello";
+const name = "World";
+
+const greetingName = `${greeting} ${name}`;
+
+console.log(greetingName);
+// console.log(`${greeting} ${name}`); // "Hello World" 출럭
---
+++ 02.js
@@ -0,0 +1,8 @@
+const lion = {
+    name: 'simba',
+    age: 3,
+    protagonist: true,
+    speech: 'HakunaMatata',
+}
+
+console.log(lion.speech);
