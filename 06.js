05a0417219ce7dd290a25446584310fda6f4c1a3
Subin, Han <ella0411@naver.com>
2024-07-26 15:41:25 +0900

    07.26

---
+++ 06.js
@@ -0,0 +1,10 @@
+function checkAge(age) {
+    if (age >= 19) {
+        console.log('안녕하세요');
+    } else {
+        console.log('나가주세요');
+    }
+}
+    
+    // 함수 실행
+    checkAge(19);
---
+++ 07.js
@@ -0,0 +1,7 @@
+function repeatWord(str, n) {
+    for(let i = 0; i < n; i++)
+        console.log(str);
+  }
+  
+  // 함수 실행
+  repeatWord('hello', 5);
